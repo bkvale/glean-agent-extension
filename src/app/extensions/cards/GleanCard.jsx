247fa3d5084418288e7daa6bcf4bf816528d6f22
Ben Kvale <benkvale@Bens-MacBook-Pro.local>
2025-08-07 13:04:52 -0500

    Comprehensive fix: Simplify extension ID, add description, simplify React component

--- src/app/extensions/cards/GleanCard.jsx
+++ src/app/extensions/cards/GleanCard.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Text, Box, Button } from '@hubspot/ui-extensions';
+import { Text, Box } from '@hubspot/ui-extensions';
 
 const GleanCard = () => {
-  console.log('GleanCard component loaded');
-  
   return (
     <Box padding="medium">
-      <Text variant="h3">Strategic Account Plan</Text>
+      <Text>Strategic Account Plan</Text>
       <Text>Hello from Glean Test Card!</Text>
-      <Button 
-        variant="primary" 
-        onClick={() => console.log('Button clicked')}
-      >
-        Test Button
-      </Button>
     </Box>
   );
 };
